6a1284a844ae0240d2cf3cc312f451b79f60b50a
Julian <0324108078@ut-tijuana.edu.mx>
2025-08-04 11:00:31 -0700

    agregado: 2 usuarios de ejemplo

--- src/Datos.java
+++ src/Datos.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
 
 public class Datos {
+    
     public static void crearUsuarios(ArrayList<String> usuarios) {
         usuarios.add("E.-.00000000.-.admin.-.constraseña");
         usuarios.add("E.-.00000001.-.estudiante1.-.constraseña");
         usuarios.add("D.-.00000002.-.profesor1.-.constraseña");
+        usuarios.add("D.-.00000003.-.Jorge Sandoval.-.jorge12345");
+        usuarios.add("E.-.00000004.-.Docente chido.-.claveacceso");
     }
 }
