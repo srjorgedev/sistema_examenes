c4a40995ef7072104efcc9ba947987ea7c30425d
sjorgedev <sandovl06roque@gmail.com>
2025-08-04 18:31:15 -0700

    removiendo el cambio de color de la interfaz por COMPATIBILIDAD

--- src/Interfaz.java
+++ src/Interfaz.java
@@ -10,42 +10,42 @@ public class Interfaz {
     }
 
     public static void imprimirLineaSupIzqDer() {
-        System.out.print(ANSI_BLUE + "  \u250C");
+        System.out.print("  \u250C");
         for (int i = 0; i < ancho; i++) {
             System.out.print("\u2500");
         }
-        System.out.print("\u2510\n" + ANSI_RESET);
+        System.out.print("\u2510\n");
     }
 
     public static void imprimirLineaInfIzqDer() {
-        System.out.print(ANSI_BLUE + "  \u2514");
+        System.out.print("  \u2514");
         for (int i = 0; i < ancho; i++) {
             System.out.print("\u2500");
         }
-        System.out.print("\u2518\n" + ANSI_RESET);
+        System.out.print("\u2518\n");
     }
 
     public static void imprimirLineaConexion() {
-        System.out.print(ANSI_BLUE + "  \u251C");
+        System.out.print("  \u251C");
         for (int i = 0; i < ancho; i++) {
             System.out.print("\u2500");
         }
-        System.out.print("\u2524\n" + ANSI_RESET);
+        System.out.print("\u2524\n");
     }
 
     public static void imprimirBordeIzqDer() {
-        System.out.print(ANSI_BLUE + "  \u2502");
+        System.out.print("  \u2502");
         for (int i = 0; i < ancho; i++) {
             System.out.print(" ");
         }
-        System.out.print("\u2502\n" + ANSI_RESET);
+        System.out.print("\u2502\n");
     }
 
     public static void imprimirTextoLineaSalto(String texto) {
-        System.out.print(ANSI_BLUE + "  \u2502 " + texto + " ".repeat(ancho - texto.length() - 1) + "\u2502\n" + ANSI_RESET);
+        System.out.print("  \u2502 " + texto + " ".repeat(ancho - texto.length() - 1) + "\u2502\n");
     }
 
     public static void imprimirTextoLinea(String texto) {
-        System.out.print(ANSI_BLUE + "  \u2502 " + texto + " ".repeat(ancho - texto.length() - 1) + "\u2502" + ANSI_RESET);
+        System.out.print("  \u2502 " + texto + " ".repeat(ancho - texto.length() - 1) + "\u2502");
     }
 }
